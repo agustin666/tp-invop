ad7a5f721317f673b64764a5b378df2c6d1dfb01
Federico <federico@Almendra.(none)>
2012-12-10 19:37:16 -0300

    tp

--- correcplexcorre.sh
+++ correcplexcorre.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-msg=('B&B' 'C&B Greedy' 'C&B Clique' 'C&B Greedy y Clique' 'Greedy' 'Clique' 'Greedy y Clique' )
-arr=('' '-G -X' '-K -X' '-X -K -G' '-G' '-K' '-G -K')
+msg=('B&B' 'C&B Greedy' 'C&B Clique' 'C&B Greedy y Clique' )
+arr=('' '-G -X' '-K -X' '-X -K -G')
 
 len=${#msg[@]}
 
@@ -10,32 +8,11 @@ do
     for((i=0;i<$len;i++))
     do
         echo "" >> log
+        echo "Corriendo $file con ${msg[i]}"
         echo "Corriendo $file con ${msg[i]}" >> log
         echo "./Tp -f $file -t ${arr[i]} >/dev/null" >> log
         echo "***********">>log
-        ./Tp -f $file -t ${arr[i]} >/dev/null 2>> log
+        ./Tp -f $file ${arr[i]} -t >/dev/null 2>> log
         echo "***********">>log
     done
 done
-
-file="./miplib2010-benchmark/reblock67.mps.gz"
-echo "" >> log
-echo "Corriendo $file con CPLEX " >> log
-echo "./Tp -f $file -c >/dev/null" >> log
-echo "***********">>log
-./Tp -f $file -c >/dev/null 2>> log
-echo "***********">>log
-
-echo "" >> log
-echo "Corriendo $file con Greedy " >> log
-echo "./Tp -f $file -G >/dev/null" >> log
-echo "***********">>log
-./Tp -f $file -G >/dev/null 2>> log
-echo "***********">>log
-
-echo "" >> log
-echo "Corriendo $file con B&B " >> log
-echo "./Tp -f $file  >/dev/null" >> log
-echo "***********">>log
-./Tp -f $file >/dev/null 2>> log
-echo "***********">>log
